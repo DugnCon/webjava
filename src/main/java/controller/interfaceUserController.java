737cea0796a7e45f4766479ee8f07e223f67020e
DugnCon <damdaidung9b@gmail.com>
2024-10-24 18:41:22 +0700

    update introduce

--- src/main/java/controller/interfaceUserController.java
+++ src/main/java/controller/interfaceUserController.java
@@ -3,16 +3,16 @@ package main.java.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-public class interfaceUserController {
+public class interfaceUserController extends baseSceneController {
 	@FXML
 	private Button home,introduce,suprise,service,contact;
 	@FXML
 	private void handleHome() {
-		
+		createScene(home,"/main/sources/interfaceUser.fxml","/WebJava/src/main/sources/css/interfaceUser.css");
 	}
 	@FXML
 	private void handleIntro() {
-		
+		createScene(introduce,"/main/sources/interfaceUser_1.fxml","/main/sources/css/interfaceUser.css");
 	}
 	@FXML
 	private void handleSuprise() {
