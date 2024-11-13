322871e1ceeeba6ef995c1b1774f9381fa1e8687
phamkhanhduy <phamkhanhduy669@gmail.com>
2024-11-13 17:59:04 +0700

    Revert "updating"
    This reverts commit 5562b15f18fc4225da57c15dce1a41609357d463.

--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-module WebJava {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.graphics;
-
-    opens main.java.controller to javafx.fxml;
-	opens main to javafx.graphics, javafx.fxml;
-}
