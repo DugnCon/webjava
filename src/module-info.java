cb9e0cf6325e48999fa51db297d0cbbef81b1ec1
DugnCon <damdaidung9b@gmail.com>
2024-11-13 18:06:47 +0700

    update

--- src/module-info.java
+++ src/module-info.java
@@ -2,7 +2,14 @@ module WebJava {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
+	requires java.sql;
+	requires mysql.connector.j;
+	requires java.net.http;
+	requires com.google.gson;
+	requires java.desktop;
+	requires javafx.media;
 
     opens main.java.controller to javafx.fxml;
 	opens main to javafx.graphics, javafx.fxml;
+	opens main.java.model to javafx.base;
 }
